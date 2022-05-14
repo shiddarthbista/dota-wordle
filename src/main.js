116a13f1613351a24bb03ea4b492e74bb9a60fe9
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2022-05-13 20:33:24 -0400

    Navbar
    -delete unneeded files
    -Add navbar changes
    - add eslint configurations

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+/* eslint-disable prettier/prettier */
+import { createApp } from "vue";
+import App from "./App.vue";
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount("#app");
